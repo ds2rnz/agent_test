11baaf3ae427e77a5e2ee4e715cb41f7c77f6cf4
ds2rnz <ds2rnz@naver.com>
2025-11-20 17:06:42 +0900

    Add files via upload

--- agent_test.py
+++ agent_test.py
@@ -1,104 +1,54 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
-
-from langchain_core.tools import tool
 from datetime import datetime
 import pytz
-
-from langchain_community.tools import DuckDuckGoSearchResults
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
-
+import streamlit as st
+from langchain.tools import tool
+import pytz
 from dotenv import load_dotenv
 import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from pathlib import Path
+from users_db import USERS_DB
+from main_ai_app import show_main_app     # ai agent 메인 함수
+from login_app import show_login_page, check_login      # 로그인 함수
+
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-system_prompt_text = """
-당신은 고성군청 직원을 위한 친절한 고성군청 AI 도우미입니다.
 
-1. 직원들이 질문하면 구체적이고 자세하게 설명해주세요 .
-2. 모르는 내용이면 도구를 이용하여 인터넷 검색을 꼭해서 답변해주세요.
-3. 인터넷 검색에 대하여 링크를 표시해 주세요.
-4. 이 지역은 강원도 고성군입니다.
-   - 고성군청 주소는 강원특별자치도 고성군 간성읍 고성중앙길9입니다.
-5. 강원도 고성군 관련 관광지 질문이 들어오면 아래 홈페이지를 참고하여 답해주세요.
-   - 고성군 관광포털 사이트 : https://gwgs.go.kr/tour/index.do
-6. 강원도 고성군 고성군청에 관하여 질문이 들어오면 아래 홈페이지를 참고하여 답해주세요
-   - 고성군청 홈페이지 : https://gwgs.go.kr
-7. 고성군수는 함명준입니다.
-   - 고성군수는 고성군 발전을 위하여 노력하시는분입니다.
-8. 고성군청 ai 도우미는 고성군청 총무행정관 정보관리팀에서 agent를 제작하였습니다.
-   - langchain을 기반으로 제작하였으며, RAG기술과 학습기능을 탐재하였으며, 지속적으로 기능추가 예정입니다.
-9. 한글로 답해주세요
-"""
 
 
 # 모델 초기화
 llm = ChatOpenAI(model="gpt-5")
 
 
-# def get_ai_response(messages):
-#     for chunk in llm.stream(messages):
-#         text = getattr(chunk, "content", None)
-#         if isinstance(text, str) and text:
-#            yield text
+# ==================== 메인 실행 ====================
 
-def get_ai_response(messages):
-    messages = llm.stream(messages)
-    return messages
+config = {"configurable": {"thread_id": "1"}}
 
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-large", 
+    api_key=st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
+    )
 
+# 페이지 설정
+st.set_page_config(page_title="GPT 기반 AI 도우미", page_icon="💬", layout="wide")
 
-# Streamlit 앱
-st.title("💬 GPT-4o Langchain Chat")
+# 세션 상태 초기화
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+if 'user_info' not in st.session_state:
+    st.session_state.user_info = None
 
-# 스트림릿 session_state에 메시지 저장
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        SystemMessage(content=system_prompt_text),  
-        AIMessage("How can I help you?")
-    ]
-
-# 스트림릿 화면에 메시지 출력
-for msg in st.session_state.messages:
-    if msg.content:
-        if isinstance(msg, SystemMessage):
-            st.chat_message("system").write("저는 사용자를 돕기 위해 최선을 다하는 고성군청 AI 도우미입니다")
-        elif isinstance(msg, AIMessage):
-            st.chat_message("assistant").write(msg.content)
-        elif isinstance(msg, HumanMessage):
-            st.chat_message("user").write(msg.content)
-        elif isinstance(msg, ToolMessage):
-            st.chat_message("tool").write(msg.content)
-
-
-# 사용자 입력 처리
-if prompt := st.chat_input():
-    st.chat_message("user").write(prompt) # 사용자 메시지 출력
-    st.session_state.messages.append(HumanMessage(prompt)) # 사용자 메시지 저장
-
-    response = get_ai_response(st.session_state["messages"])
-    
-    result = st.chat_message("assistant").write_stream(response) # AI 메시지 출력
-    st.session_state["messages"].append(AIMessage(result)) # AI 메시지 저장 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 로그인 상태에 따라 페이지 표시
+if not st.session_state.logged_in:
+    show_login_page()
+else:
+    show_main_app()
 
 
 
--- ai_qna_app.py
+++ ai_qna_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-from langchain.tools import tool
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.tools import DuckDuckGoSearchResults
@@ -15,59 +14,13 @@ import traceback
 import time
 import pytz
 from datetime import datetime
-from langchain.chat_models import init_chat_model
 from langchain_openai import ChatOpenAI
-from langchain.agents import create_agent
-
-@tool
-def get_current_time(timezone: str, location: str) -> str:
-    '''  해당 지역 현재시각을 구하는 함수 '''
-    try:
-        tz = pytz.timezone(timezone)
-        now = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-        result = f'{timezone} ({location}) 현재시각 {now}'
-        return result
-    except pytz.UnknownTimeZoneError:
-        return f"알 수 없는 타임존: {timezone}"  
-
-@tool
-def get_web_search(query: str) -> str:
-    """
-    웹 검색을 수행하는 함수.
-
-    Args:
-        query (str): 검색어
-    Returns:
-        str: 검색 결과
-    """
-    custom_wrapper = DuckDuckGoSearchAPIWrapper(region="kr-kr", time="y", max_results=10)
-    search = DuckDuckGoSearchResults(
-        api_wrapper=custom_wrapper,
-        source="news, image, text",
-        results_separator=';\n')
-    
-    results = search.run(query)
-
-    st.toast("웹 검색을 통아여 알아보고 있습니다.", icon="🎉")
-    return results
 
 
-
-config = {"configurable": {"thread_id": "1"}}
-
-# llm = init_chat_model(
-#     model = "openai:gpt-4o",
-#     temperature=0.6, 
-#     max_tokens=1000, 
-#     timeout=10, 
-#     max_retries=2, 
-#     )
 
 llm = ChatOpenAI(
     model = "gpt-5",
-    temperature=0.6, 
     max_tokens=1000, 
-    timeout=10, 
     )
 
 
@@ -75,14 +28,6 @@ embedding = OpenAIEmbeddings(
     model="text-embedding-3-large", 
     api_key=st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
     )
-
-agent = create_agent(
-    model=llm,
-    tools=[get_current_time, get_web_search],
-    middleware=[],
-    system_prompt="사용자가 질문을하면 구체적이고 자세하게 설명해주고 모르는 내용이면 인터넷 검색을 꼭해서 답변해줘 그리고 한글로 답해주세요", 
-    )
-
 
 
 def answer_question(query: str):
@@ -141,11 +86,7 @@ def answer_question(query: str):
                 
 
 def ai_answer(messages):
-    response = agent.invoke(
-        {"messages": messages},
-            config=config,
-            tool_choice='any'
-            )
+    response = llm.stream(messages)
     return response
 
 
