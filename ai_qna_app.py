b350a0cd477f6fe0d4a77d739718834f7b299ae1
ds2rnz <ds2rnz@naver.com>
2025-11-21 11:37:14 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -62,7 +62,7 @@ def answer_question(query: str):
                     답변:"""
 
         prompt = PromptTemplate(
-                 template="[page_content]",
+                 template={"[page_content]"},
                  input_variables=["page_content"]
                  )
         retriever = vectorstore.as_retriever(search_kwargs={"k":3})
@@ -179,6 +179,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
