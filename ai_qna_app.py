b85f795cdc0f0f54561cc9bda7c7e30db47c6c87
ds2rnz <ds2rnz@naver.com>
2025-11-21 11:33:23 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -70,7 +70,6 @@ def answer_question(query: str):
                 retriever=retriever,
                 name="policy_retriever", 
                 description="고성군청 업무 관련 문서 검색",
-                document_prompt=prompt,
                 )
         result = qa_chain.invoke(query)
         if isinstance(result, dict):
@@ -179,6 +178,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
