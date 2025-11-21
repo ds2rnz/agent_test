3399b4f6592f1063164285db25c9c467f054f2a7
ds2rnz <ds2rnz@naver.com>
2025-11-21 13:09:44 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -68,7 +68,7 @@ def answer_question(query: str):
                 input_variables=["context", "question"]
                 )
         retriever = vectorstore.as_retriever(search_kwargs={"k":3})
-        qa_chain = retrieval_qa.from_chain_type(
+        qa_chain = retrieval_qa(
                llm=llm,
                chain_type="stuff",
                retriever=retriever,
@@ -182,4 +182,5 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
